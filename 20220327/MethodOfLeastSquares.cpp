9b5e225acf444443d683e40fa9ffd72c74c9b9bc
depstate <dldudtjr0000@gmail.com>
2022-04-01 00:16:24 +0900

    20220327

---
+++ 20220327/MethodOfLeastSquares.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cmath>
+int i=0;
+float x[] = {2, 4, 6, 8};
+float y[] = {81, 93, 91, 97};
+
+
+float avg_array(float array[]){
+	float sum = 0;
+	for(i = 0; i < 4; i++){
+		sum += array[i];
+	}
+	printf("%f\n", sum/4);
+	return (sum/4);
+};
+
+float gradient_line(float array_x[], float array_y[], float avg_x, float avg_y){
+	float divisor = 0;
+	float dividend = 0;
+	
+	for(i = 0; i < 4; i++)
+	{
+		divisor += pow((array_x[i] - avg_x),2);
+		dividend += (array_x[i] - avg_x) * (array_y[i] - avg_y);
+		printf("%f, %f\t",array_x[i],array_y[i]);
+		printf("%f %f\n", divisor, dividend);
+	}
+	return dividend/divisor;
+}
+
+float yyy(float avg_x, float avg_y, float gradient){
+	return avg_y-(avg_x * gradient);
+}
+
+
+
+int main() {
+
+	float avg_x = avg_array(x);
+	float avg_y = avg_array(y);
+	float gradient= gradient_line(x, y, avg_x, avg_y);
+	float yy = yyy(avg_x, avg_y, gradient);
+	
+	printf("%f %f\n", avg_x, avg_y);
+	printf("y = %fx+ %f\n", gradient, yy);
+	
+		return 0;
+}
